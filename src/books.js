f65ced73c80626ccff04e9af23bb81c30f74eddf
Satwik-2004 <satwik1714@gmail.com>
2025-05-25 17:07:25 +0530

    Initial commit- My first React app

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from './images/Book-1.jpg';
+import img2 from './images/Book-2.jpg';
+import img3 from './images/Book-3.jpg';
+export const books = [
+    {
+        title :'The Let Them Theory',
+        author :'Mel Robbins',
+         image : img1,
+         id:1,
+    },
+    {
+        title :'Atomic Habits',
+        author :' James Clear',
+         image : img2,
+         id:2,
+    },
+    {
+        title :'Nightshade ',
+        author :' Michael Connelly',
+         image : img3,
+         id:3,
+    },
+];
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+import './index.css'
+import {books} from "./books";  
+import Book from './Book';
+
+
+const Booklist= () => {
+    
+    return(
+        <>
+        <h1>Amazon Best Sellers</h1>
+        <section className="booklist">
+            {books.map((book, index) => {
+             return <Book {...book} key={book.id} 
+             number={index}/>
+            }) }
+            </section></>
+    )
+}
+
+
+
+
+ 
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<Booklist />); 
